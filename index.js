4e876b701638dcfc765d1d135281958ba35f4e7c
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-08 19:28:59 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+// We use require() rather than import bc Node doesn't have easy compatibiliy with ES6
+const express = require("express");
+const app = express();
+
+// Route handler
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// Dynamic port listening
+// If there is an environment variable that has been defined by Heroku, use their port, otherwise 5000
+const PORT = process.env.PORT || 5000;
+// Watches for any traffic coming into port
+app.listen(PORT);
